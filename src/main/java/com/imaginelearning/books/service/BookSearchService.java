93515fffa6a5161cb3e338ab45b9153e18952a4d
VenugopalMN <111269539+VenugopalMN@users.noreply.github.com>
2026-02-26 10:48:39 +0530

    Add Log4j2 config and search request/response logging

--- src/main/java/com/imaginelearning/books/service/BookSearchService.java
+++ src/main/java/com/imaginelearning/books/service/BookSearchService.java
@@ -5,6 +5,8 @@ import com.imaginelearning.books.dto.api.BookSummary;
 import com.imaginelearning.books.dto.openlibrary.OpenLibrarySearchResponse;
 import com.imaginelearning.books.exception.BadRequestException;
 import com.imaginelearning.books.mapper.BookMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -13,6 +15,8 @@ import java.util.Objects;
 
 @Service
 public class BookSearchService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookSearchService.class);
 
     private final OpenLibraryClient openLibraryClient;
     private final BookMapper bookMapper;
@@ -29,12 +33,28 @@ public class BookSearchService {
 
         OpenLibrarySearchResponse response = openLibraryClient.search(query);
         if (response.getDocs() == null) {
+            LOGGER.info("Generated search response for query='{}' with resultCount=0", query);
             return Collections.emptyList();
         }
 
-        return response.getDocs().stream()
+        List<BookSummary> books = response.getDocs().stream()
                 .map(bookMapper::toBookSummary)
                 .filter(Objects::nonNull)
                 .toList();
+
+        if (books.isEmpty()) {
+            LOGGER.info("Generated search response for query='{}' with resultCount=0", query);
+        } else {
+            BookSummary firstBook = books.get(0);
+            LOGGER.info(
+                    "Generated search response for query='{}' with resultCount={} firstTitle='{}' firstAuthor='{}'",
+                    query,
+                    books.size(),
+                    firstBook.title(),
+                    firstBook.author()
+            );
+        }
+
+        return books;
     }
 }
